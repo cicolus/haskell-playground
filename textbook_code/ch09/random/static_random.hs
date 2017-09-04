746b4204df9544b42599d0f263aa4793298340a3
Leo Du <dul2@uw.edu>
2017-09-04 16:00:25 -0700

    random number generation programs

---
+++ textbook_code/ch09/random/static_random.hs
@@ -0,0 +1,18 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in (firstCoin, secondCoin, thirdCoin)
+
+-- example usage:
+-- threeCoins (mkStdGen 10)
+
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+    let (value, newGen) = random gen
+        (restOfList, finalGen) = finiteRandoms (n-1) newGen
+    in (value:restOfList, finalGen)
