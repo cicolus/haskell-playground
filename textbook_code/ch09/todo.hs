51f1e27016d15dc9bb6d8107a15ea9ac7445dec8
Leo Du <dul2@uw.edu>
2017-09-04 04:15:56 -0700

    add exercise bump function

--- textbook_code/ch09/todo.hs
+++ textbook_code/ch09/todo.hs
@@ -8,6 +8,7 @@ dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
 dispatch "view" = view
 dispatch "remove" = remove
+dispatch "bump" = bump
 
 main = do
     (command:argList) <- getArgs
@@ -43,3 +44,24 @@ remove [fileName, numberString] = do
             hClose tempHandle
             removeFile "todo.txt"
             renameFile tempName "todo.txt")
+
+bump :: [String] -> IO ()
+bump [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        number = read numberString
+        bumpTask = todoTasks !! number
+        newTodoItems = bumpTask : (delete bumpTask todoTasks)
+    putStrLn "These are your TO-Do items after bump up:"
+    mapM_ putStrLn $ zipWith (\n line -> show n ++ " - " ++ line)
+                     [0..] newTodoItems
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle $ unlines newTodoItems
+            hClose tempHandle
+            removeFile "todo.txt"
+            renameFile tempName "todo.txt")
+
